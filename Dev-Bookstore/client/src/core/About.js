d924c2140f72e73fd7532a35e5846ea91d1beaee
DauHuy <91195338+DauHuy@users.noreply.github.com>
2022-12-14 17:59:45 +0700

    commit

---
+++ Dev-Bookstore/client/src/core/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from '../core/Layout';
+
+const Home = () => {
+    return (
+        <Layout title="About" description="Group 6">
+            <div className="container">
+                
+            </div>            
+        </Layout>
+           
+    )
+};
+
+export default Home
